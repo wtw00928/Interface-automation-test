8dbe84a3954a49307e5ab670ae75bc254516d5d8
wtw00928 <1418668133@qq.com>
2024-09-13 14:41:18 +0800

    first commit

---
+++ PytestAutoApi-master/run.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time   : 2022/3/29 15:01
+# @Author : 余少琪
+import os
+import shutil
+import sys
+import traceback
+import pytest
+
+from utils.other_tools.models import NotificationType
+from utils.other_tools.allure_data.allure_report_data import AllureFileClean
+from utils.logging_tool.log_control import INFO
+from utils.notify.wechat_send import WeChatSend
+from utils.notify.ding_talk import DingTalkSendMsg
+from utils.notify.send_mail import SendEmail
+from utils.notify.lark import FeiShuTalkChatBot
+from utils.other_tools.allure_data.error_case_excel import ErrorCaseExcel
+from utils import config
+import requests
+
+def run():
+    # 从配置文件中获取项目名称
+    try:
+        INFO.logger.info(
+            """
+                             _    _         _      _____         _
+              __ _ _ __ (_)  / \\  _   _| |_ __|_   _|__  ___| |_
+             / _` | '_ \\| | / _ \\| | | | __/ _ \\| |/ _ \\/ __| __|
+            | (_| | |_) | |/ ___ \\ |_| | || (_) | |  __/\\__ \\ |_
+             \\__,_| .__/|_/_/   \\_\\__,_|\\__\\___/|_|\\___||___/\\__|
+                  |_|
+                  开始执行{}项目...
+                """.format(config.project_name)
+        )
+
+        # 清理旧的报告目录
+        report_dir = './report/tmp'
+        if os.path.exists(report_dir):
+            shutil.rmtree(report_dir)
+        os.makedirs(report_dir)
+
+        pytest.main(['-s', '-W', 'ignore:Module already imported:pytest.PytestWarning',
+                     '--alluredir', './report/tmp'])
+
+        os.system(r"allure generate ./report/tmp -o ./report/html --clean")
+
+        allure_data = AllureFileClean().get_case_count()
+        notification_mapping = {
+            NotificationType.DING_TALK.value: DingTalkSendMsg(allure_data).send_ding_notification,
+            NotificationType.WECHAT.value: WeChatSend(allure_data).send_wechat_notification,
+            NotificationType.EMAIL.value: SendEmail(allure_data).send_main,
+            NotificationType.FEI_SHU.value: FeiShuTalkChatBot(allure_data).post
+        }
+
+        if config.notification_type != NotificationType.DEFAULT.value:
+            notification_mapping.get(config.notification_type)()
+
+        if config.excel_report:
+            ErrorCaseExcel().write_case()
+
+        # 程序运行之后，自动启动报告
+        os.system(f"allure serve ./report/tmp -h 127.0.0.1 -p 9999")
+
+    except Exception:
+        e = traceback.format_exc()
+        send_email = SendEmail(AllureFileClean.get_case_count())
+        send_email.error_mail(e)
+        raise
+
+
+if __name__ == '__main__':
+    run()
